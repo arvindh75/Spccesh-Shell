59cd5191582752bf2d711c7362e1704cdfccb00f
arvindh75 <iiith@arvindharun.com>
2020-08-31 15:24:50 +0530

    ls extended

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 #include <sys/stat.h>
 #include <dirent.h>
 
+#define LS_SIZE 10
+
 void str_replace(char *target, const char *needle, const char *replacement)
 {
     char buffer[1024] = { 0 };
@@ -49,10 +51,11 @@ int main()
     char buf[512];
     int maxlen=0;
     int count,pass;
-    char args[3][1000];
+    char args[LS_SIZE][1000];
     char* temp;
     int a_ls = 0;
     int l_ls = 0;
+    char* ls_dir;
 
     home = getenv("HOME");
     result = gethostname(hostname, HOST_NAME_MAX);
@@ -77,98 +80,133 @@ int main()
         printf("<%s@%s:%s> : ", username, hostname, cwd);
         scanf("%[^\n]%*c", input);
         char* inp = strtok(input, " \t");
-
-        if(!strcmp(inp, "exit")) {
-            exit = 1;
-        }
-
-        else if(!strcmp(inp, "cd")) {
-            if(!(p = strtok(NULL, " \t")))
-            {
-                p = "~";
+        if(inp != NULL) {
+            if(!strcmp(inp, "exit")) {
+                exit = 1;
             }
-            strcpy(new,p);
-            if(p[0] == '~')
-            {
-                strcpy(new, home);
-                strcat(new, p+1);
-            }
-            strcpy(fPath, new);
-            if(chdir(fPath) < 0)
-            {
-                printf("cd: Not a valid path\n");
-            }
-        }
 
-        else if(!strcmp(inp, "pwd")) {
-            printf("%s\n", tcwd);
-        }
-
-        else if(!strcmp(inp, "ls")) {
-            a_ls=0;
-            l_ls=0;
-            mydir = opendir(tcwd);
-            ldir = opendir(tcwd);
-            maxlen = 0;
-            while((myfile = readdir(ldir)) != NULL) {
-                if(myfile->d_name[0] != '.') {
-                    if(strlen(myfile->d_name) > maxlen) {
-                        maxlen = strlen(myfile->d_name);
-                    } 
+            else if(!strcmp(inp, "cd")) {
+                if(!(p = strtok(NULL, " \t")))
+                {
+                    p = "~";
+                }
+                strcpy(new,p);
+                if(p[0] == '~')
+                {
+                    strcpy(new, home);
+                    strcat(new, p+1);
+                }
+                strcpy(fPath, new);
+                if(chdir(fPath) < 0)
+                {
+                    printf("cd: Not a valid path\n");
                 }
             }
-            count = 0;
-            pass=0;
-            temp = "";
-            for(int j=0; j<3; j++)
-            {
-                temp = strtok(NULL, " \t");
-                if(temp == NULL)
+
+            else if(!strcmp(inp, "pwd")) {
+                printf("%s\n", tcwd);
+            }
+
+            else if(!strcmp(inp, "ls")) {
+                a_ls=0;
+                l_ls=0;
+                count = 0;
+                pass=0;
+                temp = "";
+
+                for(int j=0; j < LS_SIZE; j++)
                 {
-                    for(int k = j; k < 3; k++)
+                    temp = strtok(NULL, " \t");
+                    if(temp == NULL)
                     {
-                        strcpy(args[k], "\0");
-                    }
-                    break;
-                }
-                int i = 0;
-                for(i = 0; temp[i] != '\0'; i++)
-                {
-                    args[j][i] = temp[i];
-                }
-                args[j][i] = '\0';
-            }
-            while((myfile = readdir(mydir)) != NULL)
-            {
-                if(!strcmp(args[0],"-a" ) || !strcmp(args[1],"-a" ) || !strcmp(args[2],"-a" )) {
-                    if(count == 0 && pass == 1) {
-                        printf("\n");
-                    }
-                    printf("%-*s",maxlen + 1,myfile->d_name);
-                    count=(count+1)%4;
-                    pass=1;
-                }
-                else {
-                    if(myfile->d_name[0] != '.') {
-                        if(count == 0 && pass == 1) {
-                            printf("\n");
+                        for(int k = j; k < LS_SIZE; k++)
+                        {
+                            strcpy(args[k], "\0");
                         }
-                        printf("%-*s",maxlen + 1,myfile->d_name);
-                        count=(count+1)%4;
-                        pass=1;
+                        break;
+                    }
+                    int i = 0;
+                    for(i = 0; temp[i] != '\0'; i++)
+                    {
+                        args[j][i] = temp[i];
+                    }
+                    args[j][i] = '\0';
+                    
+                }
+
+                for(int j=0; j < LS_SIZE; j++) {
+                    if(args[j] != NULL) {
+                        if(!strcmp(args[j], "-la")) {
+                            a_ls = 1;
+                            l_ls = 1;
+                        }
+                        if(!strcmp(args[j], "-a")) {
+                            a_ls = 1;
+                        }
+                        if(!strcmp(args[j], "-l")) {
+                            l_ls = 1;
+                        }
                     }
                 }
-                //sprintf(buf, "%s/%s", tcwd, myfile->d_name);
-                //stat(buf, &mystat);
-                //printf("%zu",mystat.st_size);
-                //printf(" %s\n", myfile->d_name);
-            }
-            closedir(mydir);
-            printf("\n");
-        }
 
-        else {
-            printf("Command not found !\n");
+                for(int j=0;j < LS_SIZE; j++) {
+                    if(args[j] != NULL) {
+                        if((args[j][0] >= 97 && args[j][0] <= 122) || (args[j][0] >= 65 && args[j][0] <= 90) || (args[j][0] == 46) || (args[j][0] == 47)) {
+                            if(args[j][0] == '~')
+                                ls_dir = home;
+                            else
+                                ls_dir = args[j];
+                            mydir = opendir(ls_dir);
+                            if(mydir == NULL) {
+                                printf("\nCannot open the Directory %s",ls_dir);
+                                continue;
+                            }
+                            ldir = opendir(ls_dir);
+                            printf("\n%s :\n", ls_dir);
+                            maxlen = 0;
+                            while((myfile = readdir(ldir)) != NULL) {
+                                if(myfile->d_name[0] != '.') {
+                                    if(strlen(myfile->d_name) > maxlen) {
+                                        maxlen = strlen(myfile->d_name);
+                                    } 
+                                }
+                            }
+                            while((myfile = readdir(mydir)) != NULL)
+                            {
+                                if(a_ls == 1) {
+                                    if(count == 0 && pass == 1) {
+                                        printf("\n");
+                                    }
+                                    printf("%-*s",maxlen + 1,myfile->d_name);
+                                    count=(count+1)%4;
+                                    pass=1;
+                                }
+                                else { 
+                                    if(myfile->d_name[0] != '.') {
+                                        if(count == 0 && pass == 1) {
+                                            printf("\n");
+                                        }
+                                        printf("%-*s",maxlen + 1,myfile->d_name);
+                                        count=(count+1)%4;
+                                        pass=1;
+                                    }
+                                }
+                                //sprintf(buf, "%s/%s", tcwd, myfile->d_name);
+                                //stat(buf, &mystat);
+                                //printf("%zu",mystat.st_size);
+                                //printf(" %s\n", myfile->d_name);
+                            }
+                            closedir(mydir);
+                        }
+                    }
+                }
+
+                printf("\n");
+            }
+
+            else {
+                printf("Command not found !\n");
+            }
         }
         printf("\n");
     }
